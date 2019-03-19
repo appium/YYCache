26de4840e178dcbcc940347279811fbc23a10628
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-03-19 17:53:29 +0900

    add tvOS

---
+++ Framework/YYCache tvOS/YYCache_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  YYCache_tvOS.h
+//  YYCache tvOS
+//
+//  Created by Kazuaki Matsuo on 2019/03/19.
+//  Copyright © 2019 ibireme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YYCache_tvOS.
+FOUNDATION_EXPORT double YYCache_tvOSVersionNumber;
+
+//! Project version string for YYCache_tvOS.
+FOUNDATION_EXPORT const unsigned char YYCache_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YYCache_tvOS/PublicHeader.h>
+
+
